e0a1127cb6e2ba3f57991da38bb43e55f7724516
Park Seonhui <fool0116@naver.com>
2023-01-29 11:43:58 +0900

    eslint set up

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,20 @@
+<!-- var appHeader = {
+  template: "<div>header</div>",
+  methods: {
+    addNum: function(){
+    }
+  }
+} -->
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style></style>
