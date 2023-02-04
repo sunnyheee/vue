82c23ac5cc9197729de44f8d65ed7fe0bb107546
ソニ <pakusonhi@soniui-MacBookea.local>
2023-02-04 16:38:30 +0900

    [v-model,v-on:click]TodoInput methods

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      newTodoItem:""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      // save 
+      localStorage.setItem(this.newTodoItem,this.newTodoItem); 
+      this. newTodoItem = "";
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
