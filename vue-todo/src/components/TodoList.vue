31d100f5c278489271bea1584e37b482dc7c0c3e
sunnyheee <fool0116@naver.com>
2023-02-18 13:08:59 +0900

    push list

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>list</div>
+  <div>
+    <ul>
+      <li v-for="todoItems in todoItems" v-bind:key="todoItems">
+        {{ todoItems }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function(){
+    return { 
+      todoItems: []
+    }
+  },
+  created: function(){
+    if(localStorage.length > 0){
+      for(var i = 0; i < localStorage.length; i++){
+        if(localStorage.key(i) !== "logolevel:webpack-dev-server")
+        this.todoItems.push(localStorage.key(i))
+        // console.log(localStorage.key(i));
+      }
+    }  
+  }
 }
 </script>
 
