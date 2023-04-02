b5f006dbdc576da62603b9c91f314f1ed07d67af
sunnyheee <fool0116@naver.com>
2023-04-02 19:14:33 +0900

    [ES6 refactoring]

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -25,7 +25,7 @@ export default {
   },
   methods: {
     addOneItem: function (todoItem) {
-      var obj = { completed: false, item: todoItem };
+      const obj = { completed: false, item: todoItem };
       localStorage.setItem(this.todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
@@ -48,7 +48,7 @@ export default {
   },
   created: function () {
     if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
+      for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
           this.todoItems.push(
             JSON.parse(localStorage.getItem(localStorage.key(i)))
